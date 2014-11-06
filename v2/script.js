3cca4b2e97cc41cb3db3752d235b773a914f6cc2
raydipasupil <raydipasupil@gmail.com>
2014-11-05 18:13:26 -0800

    lots of prototype testing

---
+++ v2/script.js
@@ -0,0 +1,13 @@
+// var modal = $('.modal');
+
+// $( ".btn" ).on( "click", function() {
+//   $( modal ).toggleClass('modal-show');
+// });
+
+// $( ".overlay" ).on( "click", function() {
+//   $( modal ).toggleClass('modal-show');
+// });
+
+// $( ".modal_close" ).on( "click", function() {
+//   $( modal ).toggleClass('modal-show');
+// });  
